3f06e9f588028ed3543d1fb1eb4606064593f12d
nguacon01 <dungdm6191@gmail.com>
2021-08-23 08:45:25 +0000

    change config, check if connected to db with get_all_table_names function

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from .engine import Engine
 from .base_model import Model
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import inspect
 
 class db_connector(object):
     def __init__(self, config_file_path):
@@ -11,9 +12,24 @@ class db_connector(object):
         self.Base = declarative_base()
         __Session = sessionmaker(bind=self.engine)
         self.session = __Session()
+        self.inspector = inspect(self.engine)
     
     def create_all(self):
         return self.Base.metadata.create_all(self.engine)
 
     def __repr__(self):
         return f"{self.config_file_path}"
+    
+    def get_schemas(self):
+        return self.inspector.get_schema_names()
+    
+    def get_all_table_names(self):
+        table_names = []
+        schemas = self.get_schemas()
+        for schema in schemas:
+            print("schema: %s" % schema)
+            print("*"*50)
+            for table_name in self.inspector.get_table_names(schema=schema):
+                print(f'table: {table_name}')
+                table_names.append(table_name)
+        return table_names
--- engine.py
+++ engine.py
@@ -16,12 +16,12 @@ class Engine:
             create engine, connect to the database
         """
         self.engine = create_engine(
-                    f"mysql+pymysql://{self.config['MYSQL_USER']}:{self.config['MYSQL_PASSWORD']}@{self.config['MYSQL_HOST']}:{self.config['MYSQL_PORT']}/{self.config['MYSQL_DATABASE']}",
+                    f"{self.config['SQL_TYPE_DB']}://{self.config['SQL_USER']}:{self.config['SQL_PASSWORD']}@{self.config['SQL_HOST']}:{self.config['SQL_PORT']}/{self.config['SQL_DATABASE']}",
                     # echo=True
                 )
 
     def __repr__(self):
         # return str(self.config)
-        return f"<Engine:{self.config['MYSQL_USER']} - {self.config['MYSQL_HOST']} - {self.config['MYSQL_DATABASE']}"
+        return f"<Engine:{self.config['SQL_USER']} - {self.config['SQL_HOST']} - {self.config['SQL_DATABASE']}"
 
     __str__ = __repr__
