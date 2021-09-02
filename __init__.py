5633f93e01cace2246f62b53e4387ca9081c1107
nguacon01 <dungdm6191@gmail.com>
2021-09-02 08:37:00 +0000

    fig bug can not read config file

--- __init__.py
+++ __init__.py
@@ -1,21 +1,40 @@
-from .engine import Engine
-from .base_model import Model
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import inspect
+import os
+
+from .engine import Engine
+from .base_model import Model
+from .configuration import Config
 
 class db_connector(object):
     def __init__(self, config_file_path):
         self.config_file_path = config_file_path
-        self.engine = Engine(self.config_file_path).engine
         self.Model = Model
         self.Base = declarative_base()
-        __Session = sessionmaker(bind=self.engine)
-        self.session = __Session()
-        self.inspector = inspect(self.engine)
+
+    @property
+    def config(self):
+        if not os.path.isfile(self.config_file_path):
+            raise IOError('Config file not accessible')
+        return Config(self.config_file_path).ConfMap
+
+    @property
+    def engine(self):
+        return Engine(self.config).engine
+
+    @property
+    def session(self):
+        return sessionmaker(bind=self.engine)
+    
+    @property
+    def inspector(self):
+        return inspect(self.engine)
     
     def create_all(self):
         return self.Base.metadata.create_all(self.engine)
+    # def drop_all(self):
+    #     return self.Base.metadata.drop_all(self.engine)
 
     def __repr__(self):
         return f"{self.config_file_path}"
--- engine.py
+++ engine.py
@@ -6,17 +6,24 @@ class Engine:
         create engine to connect to a database based on the config
         by default, engine uses the dev configs from config object
     """
-    engine = ''
-    def __init__(self, config_file_path, env='DEV'):
-        self.config = Config(config_file_path, env=env).ConfMap
-        self.__engine_db()
+    def __init__(self, config:dict):
+        self.config = config
 
-    def __engine_db(self):
+    @property
+    def engine(self):
         """
             create engine, connect to the database
         """
-        self.engine = create_engine(
-                    f"{self.config['SQL_TYPE_DB']}://{self.config['SQL_USER']}:{self.config['SQL_PASSWORD']}@{self.config['SQL_HOST']}:{self.config['SQL_PORT']}/{self.config['SQL_DATABASE']}",
+        # by defautl, if there is no type of your db, it will be mysql
+        db_type = self.config.get('SQL_TYPE_DB', 'mysql').lower()
+
+        db_list = {
+            'mysql':'mysql+pymysql',
+            'postgres':'postgresql'
+        }
+
+        return create_engine(
+                    f"{db_list[db_type]}://{self.config['SQL_USER']}:{self.config['SQL_PASSWORD']}@{self.config['SQL_HOST']}:{self.config['SQL_PORT']}/{self.config['SQL_DATABASE']}",
                     # echo=True
                 )
 
