72ada41beb2d362df3c62aacefbed49953160755
nguacon01 <dungdm6191@gmail.com>
2022-06-02 09:43:39 +0000

    add variable to show sql query or not

--- engine.py
+++ engine.py
@@ -5,10 +5,14 @@ class Engine:
     """
         create engine to connect to a database based on the config
         by default, engine uses the dev configs from config object
+        @param config_file_path (str) : full path to config file
+        @param echo (bool) : expose sql query
+        @param env (str) : 'DEV' or 'PROD' which are config types in config file
     """
     engine = ''
-    def __init__(self, config_file_path, env='DEV'):
+    def __init__(self, config_file_path, echo=False, env='DEV'):
         self.config = Config(config_file_path, env=env).ConfMap
+        self.echo = echo
         self.__engine_db()
 
     def __engine_db(self):
@@ -17,7 +21,7 @@ class Engine:
         """
         self.engine = create_engine(
                     f"{self.config['SQL_TYPE_DB']}://{self.config['SQL_USER']}:{self.config['SQL_PASSWORD']}@{self.config['SQL_HOST']}:{self.config['SQL_PORT']}/{self.config['SQL_DATABASE']}",
-                    # echo=True
+                    echo=self.echo
                 )
 
     def __repr__(self):
